2b6ff055939d5bec184d5888528d440b0da27ebf
sohagsrz <shaadv37@gmail.com>
2024-05-16 21:11:29 +0600

    final

--- api/index.py
+++ api/index.py
@@ -2,8 +2,10 @@ from sanic import Sanic
 from sanic.response import json
 app = Sanic()
 
- 
+
 @app.route('/')
+async def index(request):
+    return json({'hello': 'world'})
 @app.route('/<path:path>')
 async def index(request, path=""):
     return json({'hello': path})
